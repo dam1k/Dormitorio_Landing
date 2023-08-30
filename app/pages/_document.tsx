bb8a711b1a5bdf81faf96ca2226643424d1894b5
damik <damianbostan12@gmail.com>
2023-08-30 16:46:13 +0300

    custom document

---
+++ app/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function _document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="mailru-domain" content="6ADAv6HZA8DH8vYy" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default _document;
